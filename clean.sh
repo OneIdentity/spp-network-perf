ef09cc04adf4ba540223a05366d7a7831efb9d50
petrsnd <petrsnd@gmail.com>
2019-10-17 15:59:52 -0600

    clean up script

---
+++ clean.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ContainerName=spp-network-perf-runtime
+
+print_usage()
+{
+    cat <<EOF
+USAGE: clean.sh [-h]
+
+  -h  Show help and exit
+
+EOF
+    exit 0
+}
+
+while getopts ":h" opt; do
+    case $opt in
+    h)
+        print_usage
+        ;;
+    ?)
+        break
+        ;;
+    esac
+done
+
+if test -t 1; then
+    YELLOW='\033[1;33m'
+    NC='\033[0m'
+fi
+
+
+# Make sure docker is installed
+if [ -z "$(which docker)" ]; then
+    >&2 echo "You must install docker to use this script"
+    exit 1
+fi
+
+
+# Check to see if spp-network-perf-runtime is running and stop it
+docker ps | grep $ContainerName
+if [ $? -eq 0 ]; then
+    $ScriptDir/stop-service.sh
+fi
+
+# Clean up any old container with that name
+docker ps -a | grep spp-network-perf-runtime
+if [ $? -eq 0 ]; then
+    docker rm $ContainerName
+fi
+
+# Remove spp-network-perf image
+docker rmi spp-network-perf
+
