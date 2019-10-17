75564fe10f36ddf80be57fcec2a2cc7a3c69bf57
petrsnd <petrsnd@gmail.com>
2019-10-17 14:15:18 -0600

    debug script for entering the container

---
+++ debug-service.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ContainerName=spp-network-perf-runtime
+
+print_usage()
+{
+    cat <<EOF
+USAGE: debug-service.sh [-h]
+  This script will attempt to attach to the running container to allow you to
+  debug what is going on inside.
+
+  -h  Show help and exit
+
+EOF
+    exit 0
+}
+
+while getopts ":h" opt; do
+    case $opt in
+    h)
+        print_usage
+        ;;
+    ?)
+        break
+        ;;
+    esac
+done
+
+if test -t 1; then
+    YELLOW='\033[1;33m'
+    NC='\033[0m'
+fi
+
+# Make sure docker is installed
+if [ -z "$(which docker)" ]; then
+    >&2 echo "You must install docker to use this script"
+    exit 1
+fi
+
+# Stop spp-network-perf-runtime if running
+docker ps | grep spp-network-perf-runtime
+if [ $? -ne 0 ]; then
+   >&2 echo "The container is not running"
+   exit 1
+else
+   echo -e "${YELLOW}Entering container${NC}"
+   docker exec -it $ContainerName /bin/bash
+fi
+
